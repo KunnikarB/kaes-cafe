bea512bb7b14934cb236f3a432bb1b426b11f117
kunnikarB <kunnikarboon@gmail.com>
2026-05-19 11:45:06 +0200

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,60 +6,63 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-overlay">
-        <div className="footer-container footer-card">
-          <div className="footer-links">
-            <h2>Quick Links</h2>
+        <div>
+          <div className="footer-container footer-card">
+            <div className="footer-links">
+              <h2>Quick Links</h2>
 
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/menu">Menu</a>
-            <a href="/gallery">Gallery</a>
-            <a href="/contact">Contact</a>
-          </div>
-          <div className="footer-contact">
-            <div className="footer-brand">
-              <h2>Visit Us</h2>
-              <img src={footerLogo} alt="Kae's caffe logo" />
+              <a href="/">Home</a>
+              <a href="/about">About</a>
+              <a href="/menu">Menu</a>
+              <a href="/gallery">Gallery</a>
+              <a href="/contact">Contact</a>
             </div>
-            <p>25 Rue de Paris, France</p>
-            <p>hello@kaescafe.com</p>
+            <div className="footer-contact">
+              <div className="footer-brand">
+                <h2>Visit Us</h2>
+                <img src={footerLogo} alt="Kae's caffe logo" />
+              </div>
+              <p>25 Rue de Paris, France</p>
+              <p>hello@kaescafe.com</p>
+              <p>
+                Mon - Sun <span>✿</span> 8AM - 10PM
+              </p>
+            </div>
+            <div className="footer-socials">
+              <h2>Follow Us</h2>
+
+              <div className="social-icons">
+                <a href="/">
+                  <FaInstagram className="icon" />
+                  Instagram
+                </a>
+
+                <a href="/">
+                  <FaPinterest className="icon" />
+                  Pinterest
+                </a>
+
+                <a href="/">
+                  <FaFacebook className="icon" />
+                  Facebook
+                </a>
+
+                <a href="/">
+                  <FaTiktok className="icon" />
+                  TikTok
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="footer-bottom ">
             <p>
-              Mon - Sun <span>✿</span> 8AM - 10PM
+              {' '}
+              <span>©</span> 2026 Kae's Café <span>✿</span> Made with ♡ in Paris{' '}
+              <span>✿</span>{' '}
             </p>
           </div>
-          <div className="footer-socials">
-            <h2>Follow Us</h2>
-
-            <div className="social-icons">
-              <a href="/">
-                <FaInstagram className="icon" />
-                Instagram
-              </a>
-
-              <a href="/">
-                <FaPinterest className="icon" />
-                Pinterest
-              </a>
-
-              <a href="/">
-                <FaFacebook className="icon" />
-                Facebook
-              </a>
-
-              <a href="/">
-                <FaTiktok className="icon" />
-                TikTok
-              </a>
-            </div>
-          </div>
         </div>
-        <div className="footer-bottom ">
-          <p>
-            {' '}
-            <span>©</span> 2026 Kae's Café <span>✿</span> Made with ♡ in Paris{' '}
-            <span>✿</span>{' '}
-          </p>
-        </div>
+        
       </div>
     </footer>
   );
